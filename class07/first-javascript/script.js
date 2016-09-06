b35321c9069d6fd438994231aec16c1d495089ef
ccattie <chris.cattie@gmail.com>
2016-09-06 18:38:18 -0400

    as

---
+++ class07/first-javascript/script.js
@@ -0,0 +1,31 @@
+alert("You are fired. Sorry bro.");
+
+document.write(10 * 10 + 2);
+
+document.write('<br>');
+
+document.write (10 / 10);
+
+document.write('<br>');
+
+document.write(50 - 17);
+
+document.write('<br>');
+
+document.write(50 + 17);
+
+document.write('<br>');
+
+document.write('chris' + 'cattie')
+
+document.write('<br>');
+
+var num1 = 10
+
+document.write(num1);
+
+document.write('<br>');
+
+function doSomething(){
+	console.log('this is working');
+}
