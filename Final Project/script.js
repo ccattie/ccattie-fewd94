df424dc1f53c28a4442b0e0cf9bc5473656176d6
ccattie <chris.cattie@gmail.com>
2016-10-04 21:27:01 -0400

    df

--- Final Project/script.js
+++ Final Project/script.js
@@ -3,3 +3,78 @@ $('#blog1').hide();
 $('#blog1li').click(function(){
 	$('#blog1').slideToggle();
 })
+
+
+
+$('#blog2').hide();
+
+$('#blog1li').click(function(){
+	$('#blog2').slideToggle();
+})
+
+
+
+$('#blog3').hide();
+
+$('#blog1li').click(function(){
+	$('#blog3').slideToggle();
+})
+
+
+
+$('#blog4').hide();
+
+$('#blog1li').click(function(){
+	$('#blog4').slideToggle();
+})
+
+
+
+$('#blog5').hide();
+
+$('#blog1li').click(function(){
+	$('#blog5').slideToggle();
+})
+
+
+
+
+$('#blog6').hide();
+
+$('#blog1li').click(function(){
+	$('#blog6').slideToggle();
+})
+
+
+
+$('#blog7').hide();
+
+$('#blog1li').click(function(){
+	$('#blog7').slideToggle();
+})
+
+
+
+
+$('#blog8').hide();
+
+$('#blog1li').click(function(){
+	$('#blog8').slideToggle();
+})
+
+
+
+$('#blog1').hide();
+
+$('#blog1li').click(function(){
+	$('#blog1').slideToggle();
+})
+
+
+
+
+$('#blog1').hide();
+
+$('#blog1li').click(function(){
+	$('#blog1').slideToggle();
+})
