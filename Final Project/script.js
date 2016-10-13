18c45a944ebd620b48e959b205be9d23d61175a7
ccattie <chris.cattie@gmail.com>
2016-10-13 00:18:52 -0400

    pdf

--- Final Project/script.js
+++ Final Project/script.js
@@ -89,7 +89,14 @@ $('#findaboardbutton').click(function(e){
 	$('#surveycontainer').empty();
 	$('#surveycontainer').prepend('<img class="keanupic" src="img/keanu.jpg">');
 	$('#surveycontainer').prepend('<p class="keanu">Great Job!  We will be in touch shortly with next steps.</p><br><br>');
-	e.preventDefault();
+  	$('html, body').animate({scrollTop:$(document).height()}, 'slow');
+  	e.preventDefault();
 });
 
-
+$('#findaboardbutton').click(function(e){
+	$('#surveycontainer2').empty();
+	$('#surveycontainer2').prepend('<img class="keanupic" src="img/keanu2.jpg">');
+	$('#surveycontainer2').prepend('<p class="keanu">Great Job!  We will be in touch shortly with next steps.</p><br><br>');
+  	$('html, body').animate({scrollTop:$(document).height()}, 'slow');
+  	e.preventDefault();
+});
